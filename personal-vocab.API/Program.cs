778c524d9c8fbdae64057d1c845904dea8a4db7f
Zuko <karatov7@gmail.com>
2024-11-04 20:20:53 +0300

    feat(appsettings): add jwt settings

--- personal-vocab.API/Program.cs
+++ personal-vocab.API/Program.cs
@@ -7,130 +7,99 @@ using personal_vocab.BLL.Logic;
 using personal_vocab.DAL.DataAccess;
 using personal_vocab.Interfeces;
 using personal_vocab.Services;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
-namespace personal_vocab
+namespace personal_vocab;
+
+public class Program()
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        private IConfiguration Configuration { get; }
+        var builder = WebApplication.CreateBuilder(args);
 
-        public Program(IConfiguration configuration)
+        #if RELEASE
+        builder.WebHost.UseUrls("http://*:80");
+        #endif
+        builder.Services.AddControllers().AddNewtonsoftJson();
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        builder.Services.AddScoped<IFlashCardService, FlashCardService>();
+        builder.Services.AddScoped<IGroupSevice, GroupService>();
+        builder.Services.AddScoped<IDeckService, DeckService>();
+        builder.Services.AddScoped<ITermService, TermService>();
+        
+        var configuration = builder.Configuration;
+
+        builder.Services.AddDbContext<DataContext>(options =>
         {
-            Configuration = configuration;
-        }
-        public static void Main(string[] args)
+            var connectionString = configuration.GetConnectionString("Db");
+            options.UseNpgsql(connectionString);
+        });
+
+        builder.Services.AddCors(c => c.AddPolicy("cors", opt =>
         {
-            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-            var builder = WebApplication.CreateBuilder(args);
+            opt.AllowAnyHeader();
+            //opt.AllowCredentials();
+            opt.AllowAnyMethod();
+            opt.AllowAnyOrigin();
+            //opt.WithOrigins(builder.Configuration.GetSection("Cors:Urls").Get<string[]>()!);
+        }));
 
-            // Add services to the container.
-            builder.WebHost.UseUrls("http://*:80");
-            builder.Services.AddControllers().AddNewtonsoftJson();
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            builder.Services.AddScoped<IFlashCardService, FlashCardService>();
-            builder.Services.AddScoped<IGroupSevice, GroupService>();
-            builder.Services.AddScoped<IDeckService, DeckService>();
-            builder.Services.AddScoped<ITermService, TermService>();
-            //builder.Services.AddDbContext<RepetitionAlgorithmContext>(options =>
-            //{
-            //    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")!);
-            //});
-            var configuration = builder.Configuration; // Get the configuration from the builder
-
-            //var connectionString = configuration.GetConnectionString("Db");
-            //builder.Services.AddDbContext<DataContext>(opt =>
-            //    opt.UseNpgsql(connectionString));
-
-            builder.Services.AddDbContext<DataContext>(options =>
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Dictionary Parser API", Version = "v1" });
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
             {
-                var connectionString = configuration.GetConnectionString("Db");
-                options.UseNpgsql(connectionString);
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
             });
+            options.OperationFilter<SecurityRequirementsOperationFilter>();
+        });
 
-            builder.Services.AddCors(c => c.AddPolicy("cors", opt =>
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
             {
-                opt.AllowAnyHeader();
-                //opt.AllowCredentials();
-                opt.AllowAnyMethod();
-                opt.AllowAnyOrigin();
-                //opt.WithOrigins(builder.Configuration.GetSection("Cors:Urls").Get<string[]>()!);
-            }));
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Personal Vocabulary API", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter a valid token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-             {
-                 {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                    new string[]{}
-                }
-                });
-
-            });
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer"),
+                ValidAudience = builder.Configuration.GetValue<string>("Jwt:Audience"),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:Secret")!))
+            };
+        });
 
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                // Configure the JwtBearer authentication options
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidIssuer = "https://localhost:5001",
-                    ValidAudience = "https://localhost:5001",
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
-                };
-            });
+        builder.Services.AddAuthorization();
 
+        var app = builder.Build();
 
-            builder.Services.AddAuthorization();
+        app.UseCors("cors");
 
-            var app = builder.Build();
+        app.UseAuthentication();
 
-            // Configure the HTTP request pipeline.
+        app.UseAuthorization();
 
-            app.UseSwagger(c =>
-            {
-                c.RouteTemplate = "personal-vocab/swagger/{documentname}/swagger.json";
-            });
+        app.UseSwagger(c =>
+        {
+            c.RouteTemplate = "personal-vocab/swagger/{documentname}/swagger.json";
+        });
 
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/personal-vocab/swagger/v1/swagger.json", "Personal Vocabulary API");
-                c.RoutePrefix = "personal-vocab/swagger";
-            });
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/personal-vocab/swagger/v1/swagger.json", "Personal Vocabulary API");
+            c.RoutePrefix = "personal-vocab/swagger";
+        });
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.UseCors(MyAllowSpecificOrigins);
-
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-
-
-            app.Run();
-        }
+        app.Run();
     }
 }
